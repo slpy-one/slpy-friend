e7f52295b3ba8e3928ef2fff2e05c33be7cd80c2
Suphakit P <me@suphakit.net>
2023-10-11 03:56:16 +0700

    feat<index>: setting up index page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,38 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { browser } from "$app/environment"
+	import { onMount } from "svelte"
+
+  let theme: string | null = ''
+  let discordID: string | null = ''
+
+  onMount(() => {
+    if (localStorage.getItem('theme') !== undefined) {
+      theme = localStorage.getItem('theme')
+    } else {
+      theme = ''
+    }
+  })
+
+  function search() {
+    if (browser) {
+      document.location.replace('/@' + discordID)
+    }
+  }
+</script>
+
+<svelte:head>
+  <title>Friends - slpy.one</title>
+
+  <meta name="description" content="Using your Discord userID and show profile :)">
+  <meta name="author" content="dethz">
+</svelte:head>
+
+<div id="indexMain">
+	<div class="card">
+    <input type="text" placeholder="Discord ID" bind:value={discordID} />
+    <br>
+    <button on:click={search}>
+      Search
+    </button>
+  </div>
+</div>
